2dc2d6015ed41b66be6145b5de7f560cfbbb45ab
igorlacerdak <igorlacerdasantos@hotmail.com>
2022-06-22 21:32:19 -0300

    Primeiro commit - Aula 01 / Aula 02

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { gql, useQuery } from "@apollo/client"
+import { client } from "./lib/apollo"
+import { Event } from "./pages/Event"
+
+
+function App() {
+  return (
+   <> 
+      <Event />
+   </>
+  )
+}
+
+export default App
